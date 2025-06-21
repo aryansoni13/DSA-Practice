11cd49a11bb590f6b6e5057d9f513b2232482e2a
ARYAN SONI <arusoni477@gmail.com>
2025-06-21 11:51:12 +0530

    added  few files

---
+++ Arrays/Rotate Array by One/main.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int i = 0, j = n - 1;
+        while (i != j) {
+            swap(arr[i], arr[j]);
+            i++;
+        }   
+        
+    }
+};
