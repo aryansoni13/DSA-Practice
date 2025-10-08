51ec097bee9c44aa854632dd73f66b714cd39ae9
Aryan Soni <127400418+aryansoni13@users.noreply.github.com>
2025-10-08 15:59:09 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
+        for (int i =0; i<n;i++){
+            if(nums[i]!=0){
                 swap(nums[i], nums[count]);
                 count++;
             }
